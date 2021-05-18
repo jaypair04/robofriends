13fd8d8851113c9d2e335b387ef24fdcc08ad3c8
Jason Pair <jaypair04@gmail.com>
2021-05-18 15:16:23 -0500

    from classes to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {  //smart components have class syntax.
-    constructor() {
-        super()
-        this.state = {  //two states because there is a state the App component needs to be a class in order to use constructor.
-            robots: [],  //these two states are the items that change in the app. The virtual DOM collects this and renders it to pass to the components.
-            searchfield: ''
-        }
-    }
-
-    componentDidMount() {
+function App() {  //these two states are the items that change in the app. The virtual DOM collects this and renders it to pass to the components.
+    const [robots, setRobots] = useState([])  //two states because there is a state the App component needs to be a class in order to use constructor.
+    const [searchfield, setSeachfield] = useState('') 
+    
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => this.setState({ robots: users }));
-    }
-
+        .then(users => setRobots( users ));
+    })
     //we create this onSearchChange and it is not part of react so the arrow function syntax is required
-    onSearchChange = (event) => {  //on a searchbox change (when you type) it is passed to the component SearchBox. When there is a change it tell the App to run the function.
-        this.setState({ searchfield: event.target.value })  //function runs on change and updates the searchfield state up in the constructor.
+    const onSearchChange = (event) => {  //on a searchbox change (when you type) it is passed to the component SearchBox. When there is a change it tell the App to run the function.
+        setSeachfield( event.target.value )  //function runs on change and updates the searchfield state up in the constructor.
     }
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {  //when the searchfield in the constructor changes it filters the robots
+    const filteredRobots = robots.filter(robot => {  //when the searchfield in the constructor changes it filters the robots
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        if (!robots.length) {
-            return <h1>Loading...</h1>
-        } 
-        else {
-            return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundry>
-                        <CardList robots={filteredRobots}/>
-                    </ErrorBoundry>
-                </Scroll>  
-            </div>
-            );
-        } 
-    }
+        })   
+    return !robots.length ?
+        <h1>Loading...</h1> : 
+        (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
+            </Scroll>  
+        </div>
+        );
 }
 export default App
